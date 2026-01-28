d10e51a429183fe0da632cd94e01442f365ca260
LeafTrace <1449070025@qq.com>
2026-01-29 00:37:39 +0800

    Lab 5: Disjoint Sets-2026.01.29

--- lab05/src/UnionFind.java
+++ lab05/src/UnionFind.java
@@ -1,29 +1,36 @@
+import org.knowm.xchart.style.markers.Oval;
+
+
 public class UnionFind {
     // TODO: Instance variables
-
+    private int[] p;
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
         // TODO: YOUR CODE HERE
+        p = new int[N];
+        for (int i =0; i < N; i++){
+            p[i] = -1;
+        }
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return Math.abs(p[find(v)]);
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return p[v];
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO: YOUR CODE HERE
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
@@ -31,7 +38,13 @@ public class UnionFind {
        function, throw an IllegalArgumentException. */
     public int find(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        validate(v);
+
+        if (p[v] < 0){
+            return v;
+        }
+        p[v] = find(p[v]);
+        return p[v];
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -41,6 +54,25 @@ public class UnionFind {
        already connected should not change the structure. */
     public void union(int v1, int v2) {
         // TODO: YOUR CODE HERE
+        int v1Size = sizeOf(v1), v2Size = sizeOf(v2);
+        int root1 = find(v1), root2 = find(v2);
+        if (root1 == root2){
+            return;
+        }
+
+        if (v1Size <= v2Size){
+            p[root2] += p[root1];
+            p[root1] = root2;
+        }
+        else{
+            p[root1] += p[root2];
+            p[root2] = root1;
+        }
     }
 
+    private void validate(int v){
+        if (v < 0 || v >p.length){
+            throw new IllegalArgumentException("\"Index \" + v + \" is out of bounds!");
+        }
+    }
 }
